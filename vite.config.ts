806689995419ec2c67569e7ac29e166d081b1b5f
周松 <zhous0310@gmail.com>
2024-09-08 17:47:01 +0800

    fix: 开发环境增加cesium插件

--- vite.config.ts
+++ vite.config.ts
@@ -5,33 +5,36 @@ import dts from 'vite-plugin-dts'
 import cesium from 'vite-plugin-cesium'
 import ElementPlus from 'unplugin-element-plus/vite'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    dts({
-      insertTypesEntry: true,
-      cleanVueFileName: true,
-      include: ['package/**/*']
-    }),
-    ElementPlus({})
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.vue'],
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '~': path.resolve(__dirname, './package')
-    }
-  },
-  build: {
-    outDir: 'lib',
-    lib: {
-      entry: resolve(__dirname, 'package/index.ts'),
-      name: 'ZCesium',
-      fileName: (format) => `ZCesium.${format}.js`
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      vue(),
+      dts({
+        insertTypesEntry: true,
+        cleanVueFileName: true,
+        include: ['package/**/*']
+      }),
+      mode !== 'production' && cesium(),
+      ElementPlus({})
+    ],
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.vue'],
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '~': path.resolve(__dirname, './package')
+      }
     },
-    rollupOptions: {
-      external: ['cesium'],
+    build: {
+      outDir: 'lib',
+      lib: {
+        entry: resolve(__dirname, 'package/index.ts'),
+        name: 'ZCesium',
+        fileName: (format) => `ZCesium.${format}.js`
+      },
+      rollupOptions: {
+        external: ['cesium', 'vue'],
 
+      }
     }
   }
 })
